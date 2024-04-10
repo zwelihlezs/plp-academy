cef233952a4af21a6565b06699764b5221ce939a
Zwelihle <zwelihle.zs@gmail.com>
2024-04-10 19:57:21 +0200

    PLP Academy

---
+++ python/week1/user_input.py
@@ -0,0 +1,7 @@
+name = input("What's your name?")
+
+age = input("How old are you?")
+
+location = input("Where are you located?")
+
+print(f"HI {name} nice to meet a {age} year old who live in {location} ;)")
