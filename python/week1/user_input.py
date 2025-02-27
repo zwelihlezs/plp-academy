8bf62907c04f5f346aa94c73d655919ce30333a9
Zwelihle Nhlapho <107548375+zwelihlezs@users.noreply.github.com>
2025-02-27 09:20:11 +0200

    Delete python/week1 directory

--- python/week1/user_input.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your name?")
-
-age = input("How old are you?")
-
-location = input("Where are you located?")
-
-print(f"HI {name} nice to meet a {age} year old who live in {location} ;)")
